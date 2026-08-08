12059a9c299e9989c66ed159df5286a9a57a142a
Nitin Kumar <nitin.kumar14@nagarro.com>
2026-08-08 07:31:08 +0530

    quiz video generator

---
+++ README.md
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "quiz video generator"
+git branch -M main
+git remote add origin https://github.com/Nkm17000/quiz_vedio_generator.git
+git push -u origin main
---
+++ main.py
@@ -0,0 +1,93 @@
+import os
+import imgkit
+from moviepy.editor import ImageClip, concatenate_videoclips
+
+# CONFIG
+OUTPUT_VIDEO = "quiz_video.mp4"
+DURATION = 3
+config = imgkit.config(wkhtmltoimage='/usr/bin/wkhtmltoimage')
+
+quiz = [
+    {
+        "question": "What is the capital of Japan?",
+        "options": ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "Largest planet in solar system?",
+        "options": ["Earth", "Mars", "Jupiter", "Venus"],
+        "answer": "Jupiter"
+    }
+]
+
+CSS = """
+<style>
+body {
+background: radial-gradient(circle at center, #0A254F, #071A3D);
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+font-family: Poppins;
+}
+.quiz-card {
+width: 1000px;
+padding: 40px;
+border-radius: 30px;
+border: 2px solid #22D3EE;
+}
+.option {
+padding: 20px;
+margin: 10px;
+border-radius: 40px;
+border: 2px solid #3B82F6;
+}
+</style>
+"""
+
+def create_html(q, index):
+    labels = ["A", "B", "C", "D"]
+
+    options_html = ""
+    for i, opt in enumerate(q["options"]):
+        options_html += f"<div class='option'>{labels[i]}. {opt}</div>"
+
+    return f"""
+    <html>
+    <head>{CSS}</head>
+    <body>
+    <div class="quiz-card">
+        <h2>Q{index+1}. {q['question']}</h2>
+        {options_html}
+    </div>
+    </body>
+    </html>
+    """
+
+images = []
+
+# Generate slides
+for i, q in enumerate(quiz):
+    file = f"slide_{i}.png"
+    imgkit.from_string(create_html(q, i), file, config=config)
+    images.append(file)
+
+# Answer slide
+answer_html = "<h1>Answers</h1>"
+for i, q in enumerate(quiz):
+    answer_html += f"<h3>Q{i+1}: {q['answer']}</h3>"
+
+imgkit.from_string(answer_html, "answer.png", config=config)
+images.append("answer.png")
+
+# Create video
+clips = [ImageClip(img).set_duration(DURATION) for img in images]
+video = concatenate_videoclips(clips)
+video.write_videofile(OUTPUT_VIDEO, fps=24)
+
+# Cleanup
+for img in images:
+    os.remove(img)
+
+print("✅ Video Created!")
